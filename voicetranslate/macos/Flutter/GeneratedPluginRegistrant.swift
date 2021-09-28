f36ac81940ff0f36751e484577d116c67c2b2548
kitoko2 <dogbo804@gmail.com>
2021-09-28 21:22:59 +0000

    application de traduction

--- voicetranslate/macos/Flutter/GeneratedPluginRegistrant.swift
+++ voicetranslate/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import flutter_tts
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
